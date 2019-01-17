a0a48a1af0fc009e72375c200de07490a4411634
Willy Ko <wko@blockchainfoundry.co>
2019-01-17 15:15:27 -0800

    Removed syscoin-js and just use request npm package

--- index.js
+++ index.js
@@ -2,86 +2,151 @@
 
 const axios = require('axios');
 const Web3 = require('web3');
+const request = require('request');
+
+/* 
+ *  Usage:  Subscribe to Geth node and push header to syscoin via RPC 
+ *
+ */
+
 let argv = require('yargs')
      .usage('Usage: $0 -sysrpcuser [username] -sysrpcpw [password] -sysrpcport [port] -ethrpcport [port]')
-     .default("sysrpcport", 8369)
+     .default("sysrpcport", 18369)
      .default("ethrpcport", 8546)
+     .default("sysrpcuser", "u")
+     .default("sysrpcpw", "p")
      .argv
 ;
 if (argv.sysrpcport < 0 || argv.sysrpcport > 65535) {
-    console.log('Invalid Sys RPC port');
+    console.log('Invalid Syscoin RPC port');
     exit();
 }
 if (argv.ethrpcport < 0 || argv.ethrpcport > 65535) {
-		console.log('Invalid Eth RPC port');
+		console.log('Invalid Geth RPC port');
 		exit();
 }
+const sysrpcport = argv.sysrpcport;
+const ethrpcport = argv.ethrpcport;
+const sysrpcuser = argv.sysrpcuser;
+const sysrpcpw = argv.sysrpcpw;
+/* This is an attempt to use syscoin-js 
+var customHttpAgent = "";
+let syscoinClient = new SyscoinRpcClient({baseUrl : "localhost",
+                port : argv.syscoinrpcport,
+                username : "u",
+                password : "p",
+                useSsl : false,
+                timeout : 30000,
+                customHttpAgent,
+                loggerLevel : "", 
+                whitelist : [], 
+                blacklist : []});
+let info = syscoinClient.networkServices.getInfo();
+info.then((response) => response.json()).then((json) => {
+  console.info('got a response', json);
+});
+*/
 
-
+/* Initialize Geth Web3 */
 let web3 = new Web3("ws://127.0.0.1:" + argv.ethrpcport);
 var collection = [];
 
-const subscription = web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
-  if (error) return console.error(error);
-  console.log(blockHeader);
-  let obj = [blockHeader['number'],blockHeader['transactionsRoot']];
-  console.log(obj);
-  collection.push(obj);
-  console.log(collection);
-	
+/* Geth subscriber for new block headers */
+const subscription_header = web3.eth.subscribe('newBlockHeaders', (error, blockHeader) => {
+    if (error) return console.error(error);
+    console.log(blockHeader);
+    let obj = [blockHeader['number'],blockHeader['transactionsRoot']];
+    collection.push(obj);
 });
 
+/* Timer for submitting header lists to Syscoin via RPC */
 const timer = setInterval(pushToRPC, 50000);
 function pushToRPC() {
+	// Check if there's anything in the collection
 	if (collection.length == 0) {
 			console.log("collection is empty");
 			return;
-	} else {
-			let instance = axios.create({
-					timeout: 1000,
-					auth: {
-							username: 'u',
-							password: 'p'
-					},
-					method: 'post',
-			});
-
-			var CallRPC = instance.post("http://localhost:18369", {"method":"syscoinmint", "params": JSON.stringify(collection)});
-			console.log("pushing to rpc", JSON.stringify(collection));
-			console.log("result: ", CallRPC);
-			collection = [];
 	}
-}
-// unsubscribes the subscription
-subscription.unsubscribe((error, success) => {
-if (error) return console.error(error);
+	
+	// Request options
+	let options = {
+        url: "http://localhost:" + sysrpcport,
+        method: "post",
+        headers:
+        {
+            "content-type": "text/plain"
+        },
+        auth: {
+            user: sysrpcuser,
+            pass: sysrpcpw 
+        },
+        body: JSON.stringify( {"jsonrpc": "1.0", "id": "ethheader_update", "method": "syscoinsetethheaders", "params": [collection]})
+    };
+
+    request(options, (error, response, body) => {
+        if (error) {
+            console.error('An error has occurred: ', error);
+        } else {
+            console.log('Post successful: response: ', body);
+        }
+    });
+
+	console.log("pushing to rpc", JSON.stringify(collection));
+};
+
+// unsubscribes the header subscription
+subscription_header.unsubscribe((error, success) => {
+    if (error) return console.error(error);
 
     console.log('Successfully unsubscribed!');
 	clearInterval(timer);
-
 });
-/*
-const sync_subscription = web3.eth.subscribe('syncing', function(error, sync){
-		console.log("sync_subscription: callback");
-//    if (!error)
-       // console.log(sync);
+
+/*  Subscription for Geth syncing status */
+const subscription_sync = web3.eth.subscribe('syncing', function(error, sync){
+	console.log("subscription_sync: callback");
+    if (!error)
+        console.log(sync);
 })
 .on("data", function(sync){
     // show some syncing stats
-    console.log("sync_subscription: on data");
+    console.log("subscription_sync: on data");
 })
 .on("changed", function(isSyncing){
-    if(isSyncing) {
-		 console.log("sync_subscription: is_syncing = true");
+   if(isSyncing) {
+   console.log("subscription_sync: is_syncing = true");
         // stop app operation
     } else {
-			console.log("sync_subscription: is_syncing = false");
+    console.log("subscription_sync: is_syncing = false");
         // regain app operation
     }
+	let options = {
+        url: "http://localhost:" + sysrpcport,
+        method: "post",
+        headers:
+        {
+            "content-type": "text/plain"
+        },
+        auth: {
+            user: sysrpcuser,
+            pass: sysrpcpw 
+        },
+        body: JSON.stringify( {"jsonrpc": "1.0", "id": "sync_update", "method": "syscoinsetethstatus", "params": [isSyncing]})
+    };
+    console.log(options.body);
+
+    request(options, (error, response, body) => {
+        if (error) {
+            console.error('An error has occurred: ', error);
+        } else {
+            console.log('Post successful: response: ', body);
+        }
+    });
 });
+
 // unsubscribes the subscription
-sync_subscription.unsubscribe(function(error, success){
+subscription_sync.unsubscribe(function(error, success){
     if(success)
         console.log('Successfully unsubscribed!');
 });
-*/
+
