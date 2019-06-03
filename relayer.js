a2b8f5b9f8dfbab2d2b24223a29934831f6820c0
sidhujag <sidhujag@gmail.com>
2019-06-02 21:10:39 -0700

    pass in datadir to relayer aswell as infura api key

--- relayer.js
+++ relayer.js
@@ -9,26 +9,15 @@ const Getter = require('./getter.js');
  *  Usage:  Subscribe to Geth node and push header to syscoin via RPC 
  *
  */
-
-/* Set up logging */
-var logFile = fs.createWriteStream('relayer-debug.log', { flags: 'a' });
-var logStdout = process.stdout;
-
-console.log = function () {
-    var date = new Date().toISOString();
-    logFile.write(date + ' '  + util.format.apply(null, arguments) + '\n');
-    logStdout.write(date + ' ' + util.format.apply(null, arguments) + '\n');
-}
-console.error = console.log;
-
-console.log("Running V1.0.9 version of the Syscoin relay logger! This tool pushed headers from Ethereum to Syscoin for consensus verification of SPV proofs of Syscoin Mint transactions.");
 /* Retrieve arguments */
 let argv = require('yargs')
-	.usage('Usage: $0 -sysrpcuser [username] -sysrpcpw [password] -sysrpcport [port] -ethwsport [port]')
+	.usage('Usage: $0 -sysrpcuser [username] -datadir [syscoin data dir] -sysrpcpw [password] -sysrpcport [port] -ethwsport [port] -infuraapi [apikey]')
 	.default("sysrpcport", 8370)
 	.default("ethwsport", 8546)
 	.default("sysrpcuser", "u")
 	.default("sysrpcpw", "p")
+	.default("datadir", "~/.syscoin")
+	.default("infurakey", "b3d07005e22f4127ba935ce09b9a2a8d")
 	.argv
 ;
 if (argv.sysrpcport < 0 || argv.sysrpcport > 65535) {
@@ -43,9 +32,24 @@ const sysrpcport = argv.sysrpcport;
 const ethwsport = argv.ethwsport;
 const sysrpcuser = argv.sysrpcuser;
 const sysrpcpw = argv.sysrpcpw;
+const datadir = argv.datadir;
+const infuraapikey = argv.infurakey;
+
+/* Set up logging */
+var logFile = fs.createWriteStream(datadir + '/syscoin-relayer.log', { flags: 'a' });
+var logStdout = process.stdout;
+
+console.log = function () {
+    var date = new Date().toISOString();
+    logFile.write(date + ' '  + util.format.apply(null, arguments) + '\n');
+    logStdout.write(date + ' ' + util.format.apply(null, arguments) + '\n');
+}
+console.error = console.log;
+
+console.log("Running V1.0.0 version of the Syscoin relay logger! This tool pushed headers from Ethereum to Syscoin for consensus verification of SPV proofs of Syscoin Mint transactions.");
 
 /* Initialize Geth Web3 */
-var infura_ws_url = "wss://rinkeby.infura.io/ws/v3/b3d07005e22f4127ba935ce09b9a2a8d";
+var infura_ws_url = "wss://rinkeby.infura.io/ws/v3/" + infuraapikey;
 var geth_ws_url = "ws://127.0.0.1:" + ethwsport;
 var web3 = new Web3(geth_ws_url);
 var web3_infura = new Web3(infura_ws_url);
